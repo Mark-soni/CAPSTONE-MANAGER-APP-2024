d2ea83fb70768a393ff4322e5c28674a33f3c470
Ignacio Fonseca <ignaciofonsecavera@outlook.com>
2024-09-29 04:02:07 -0300

    Co-authored-by: Marcelo Vivanco, Matias Fuentes

--- gestorio/src/components/footer.component.tsx
+++ gestorio/src/components/footer.component.tsx
@@ -1,6 +1,6 @@
 export function FooterComponent() {
 	return (
-		<footer className='bg-Naranjo/80 font-light text-Blanco backdrop-blur-lg text-center text-sm'>
+		<footer className='bg-Naranjo/80 font-light text-Blanco backdrop-blur-lg text-center text-sm w-full fixed bottom-0'>
 			©️ Gestorio CL - 2024
 		</footer>
 	)
